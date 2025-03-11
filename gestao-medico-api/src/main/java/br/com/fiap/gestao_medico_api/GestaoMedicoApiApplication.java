d36ddcf5defa20e53910f7691760a1b5bb074169
Diogo Weyne <162655515+diogoweyne@users.noreply.github.com>
2025-03-11 16:28:34 -0300

    Add files via upload

---
+++ gestao-medico-api/src/main/java/br/com/fiap/gestao_medico_api/GestaoMedicoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gestao_medico_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoMedicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoMedicoApiApplication.class, args);
+	}
+
+}
---
+++ gestao-medico-api/src/test/java/br/com/fiap/gestao_medico_api/GestaoMedicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gestao_medico_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoMedicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
